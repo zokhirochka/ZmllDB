bd7da4ac46aaa8a711f369caa0f9f70a95868bbc
bnjenner <bnjenner@bu.edu>
2026-04-16 16:42:42 -0400

    Added outline of RegSeqDB class

---
+++ regseqdb/regseqdb.py
@@ -0,0 +1,131 @@
+import utils
+
+
+############################################################################
+# RegseqDB Class
+class RegSeqDB:
+
+	def __init__(self):
+		'''
+		RegseqDB Class Initializations
+		'''
+
+		# MariaDB Objects
+		self.connection = None 
+		self.cursor     = None 
+
+
+	######################################
+	# Database Methods
+	def self.connect(self, host, port, database, username, password):
+		"""Connect to Database and stores connection as class attribute.
+
+		Args:
+			host: Name of host database server
+			port: Database server post 
+			database: Database to connect
+			uername: Username
+			password: Password
+
+		Returns:
+			None
+		"""
+		keys = {host, port, database, username, password}
+		self.connection, self.cursor = utils.connect_db(keys)
+		return
+
+	def __query(self, query, inputs):
+		"""Private generalized database query method
+
+		Args:
+			query: Structured SQL query template from calling function
+			inputs: Curaated or cleaned inputs from calling functions
+
+		Returns:
+			Tuple of (results, colnames, rowcount)
+		"""
+		query_output = utils.exec_query(
+            cursor=self.cursor,
+            query=query,
+            inputs=inputs
+        )
+		return (
+			query_output["results"],
+			query_output["colnames"],
+			query_output["rowcount"]
+		)
+
+
+	######################################
+	# Query Wrappers
+	def get_promoter_expr(self, promoter, condition):
+		"""Get Promoter Expression 
+
+		Args:
+			promoter: Name of promoter sequence to query
+			condition: Condition in which to grab expression values
+
+		Returns:
+			TBD
+		"""
+
+		# Check Promoter Value
+		if not utils.db_contains(self.cursor, "Promoters", "pro_name", promoter):
+			raise ValueError(f"ERROR: Promoter {promoter} not found in Promoters table.")
+		# Check Condition Value
+		if not utils.db_contains(self.cursor, "Experiments", "cond", condition):
+			raise ValueError(f"ERROR: Condition {condition} not found in Experiments table.")
+
+		# Define inputs and query
+		inputs = [promoter, condition]
+        query = """
+        		SELECT sID, num_DNA, num_RNA FROM Promoters
+        			JOIN PromoterSequences USING(pID)
+        			JOIN BarcodeCounts USING(sID)
+        			JOIN Experiments USING (eID)
+        		WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s) AND 
+        			  eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1);
+        		"""
+
+        # Submit Query
+        results, colnames, rowcount = self.__query(query, inputs)
+		return
+
+	# ######################################
+	# # Query Wrappers
+	# def get_promoter_expr_and_binding(self, promoter, condition, filter):
+	# 	"""Get Promoter Expression 
+
+	# 	Args:
+	# 		promoter: Name of promoter sequence to query
+	# 		condition: Condition in which to grab expression values
+
+	# 	Returns:
+	# 		TBD
+	# 	"""
+
+	# 	# Check Promoter Value
+	# 	if not utils.db_contains(self.cursor, "Promoters", "pro_name", promoter):
+	# 		raise ValueError(f"ERROR: Promoter {promoter} not found in Promoters table.")
+	# 	# Check Condition Value
+	# 	if not utils.db_contains(self.cursor, "Experiments", "cond", condition):
+	# 		raise ValueError(f"ERROR: Condition {condition} not found in Experiments table.")
+
+	# 	# Define inputs and query
+	# 	inputs = [promoter, condition]
+    #     query = """
+    #     		SELECT sID, num_DNA, num_RNA, affinity FROM Promoters
+    #     			JOIN PromoterSequences USING(pID)
+    #     			JOIN BarcodeCounts USING(sID)
+    #     			JOIN Experiments USING (eID)
+    #     			JOIN BindingSitesTF USING(sID)
+    #     			JOIN TranscriptionFactors USING(tID)
+    #     		WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
+    #     			AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
+    #     			AND tID = (SELECT tID FROM TranscriptionFactors WHERE tf_name = %s)
+    #     			AND num_DNA > %s;
+    #     		"""
+
+    #     # Submit Query
+    #     results, colnames, rowcount = self.__query(query, inputs)
+	# 	return
---
+++ regseqdb/utils.py
@@ -0,0 +1,81 @@
+import mariadb
+
+############################################################################
+# Database Methods
+
+def connect_db(credentials_dictionary: dict) -> tuple:
+        """Connect to MariaDB Database
+
+        Args:
+            credentials_dictionary: Dictionary containing required keys
+                - host, port, databse, username, password
+
+        Returns:
+            mariadb.connect object
+
+        Raises:
+            ValueError: Missing required keys
+        """
+
+        # Validate Keys
+        required_keys = ["host" "port", "database", "user", "password"]
+        if not all(key in credentials_dictionary for key in required_keys):
+                missing_keys = set(required_keys ) - set(credentials_dictionary.keys())
+                raise ValueError(f"ERROR: Database Connection Error. Missing parameters {missing_keys}.")
+
+        # Establish DB connection
+        connection = mariadb.connect(**credentials_dictionary)
+        cursor = connection.cursor()
+        return (connection, cursor)
+
+
+def exec_query(cursor, query, inputs):
+        """Execute Query tonMariaDB Database
+
+        Args:
+            cursor: MariaDB cursor object
+            query: Query Template
+            input: Query Inputs
+
+        Returns:
+            Dictionary containing results, colnames, and rowcounts
+
+        Raises:
+            ValueError: Invalid Query
+        """
+
+        # Execute query and catch errors
+        try:
+            cursor.execute(query, inputs)
+        except mariadb.Error as e:
+            raise ValueError(f"ERROR: Invalid SQL Query.\nQuery: {query}\nError: {e}")
+
+        # Fetch and Curate Results
+        results_dict = {
+                        "results": cursor.fetchall(),
+                        "colnames": [metadata[0] for metadata in cursor.description],
+                        "rowcount": len(results)
+                        }
+        return results_dict
+
+
+def db_contains(cursor, table, column, value):
+        """Checks if value is in MariaDB database
+
+        Args:
+            cursor: MariaDB cursor object
+            table: Table to serch for value
+            column: Column name for value search
+            value: Value to match in column
+
+        Returns:
+            True or False based on value presence
+        """
+        inputs = [table, column, value]
+        query = "SELECT * FROM %s WHERE %s = %s;"
+        results = exec_query(cursor=cursor, query=query, inputs=inputs)
+        if not results:
+                return False
+        return True
+
+
