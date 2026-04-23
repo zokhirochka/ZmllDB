d8aa7dfeab90e3cc6a69254227f0009a61dc462a
bnjenner <bnjenner@bu.edu>
2026-04-23 17:44:12 -0400

    Added Comparison Query and Promoter Binding Coodinate Query

--- project/regseqdb.py
+++ project/regseqdb.py
@@ -86,7 +86,8 @@ class RegSeqDB:
 					JOIN Experiments USING (eID)
 				WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s) AND 
 					  eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
-				GROUP BY ps.seq;
+				GROUP BY ps.seq
+				ORDER BY ;
 				"""
 
 		# Submit Query
@@ -94,14 +95,73 @@ class RegSeqDB:
 		return results
 
 
-	def get_promoter_expr_and_binding(self, promoter, condition, tf, include_rnap = True):
+	def get_promoter_binding_coords(self, promoter, condition, tf):
+		"""Get Promoter Information including MAX TF and RNAP Binding Sites
+				coordinates.
+
+		Args:
+			promoter: Name of promoter sequence to query
+			condition: Condition in which to grab expression values
+			tf: Name of TF to get binding
+
+		Returns:
+			Dictionary of {results, colnames, rowcount}
+		"""
+
+		# Check Promoter Value
+		if not utils.db_contains(self.cursor, "Promoters", "pro_name", promoter):
+			raise ValueError(f"ERROR: Promoter {promoter} not found in Promoters table.")
+		# Check Condition Value
+		if not utils.db_contains(self.cursor, "Experiments", "cond", condition):
+			raise ValueError(f"ERROR: Condition {condition} not found in Experiments table.")
+		# Check TF Value
+		if not utils.db_contains(self.cursor, "TranscriptionFactors", "tf_name", tf):
+			raise ValueError(f"ERROR: TF {tf} not found in TranscriptionFactors table.")
+
+		# Define inputs and query
+		inputs = [promoter, condition, tf, promoter, condition]
+		query = """
+				SELECT pID, pro_name, tss, seq, 
+						rnap.site_start as RNAP_start, 
+						rnap.site_stop as RNAP_stop, 
+						tf.site_start as TF_start, 
+						tf.site_stop as TF_stop
+				FROM Promoters
+				JOIN (SELECT pID, site_start, site_stop FROM Promoters
+				                JOIN PromoterSequences AS ps USING(pID)
+				                JOIN BarcodeCounts USING(sID)
+				                JOIN Experiments USING (eID)
+				                JOIN BindingSitesTF USING(sID)
+				                JOIN TranscriptionFactors USING(tID)
+				          WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
+				                AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
+				                AND tID = (SELECT tID FROM TranscriptionFactors WHERE tf_name = %s)
+				          ORDER BY affinity DESC LIMIT 1) as tf
+				USING(pID)
+				JOIN (SELECT pID, site_start, site_stop FROM Promoters
+				                JOIN PromoterSequences AS ps USING(pID)
+				                JOIN BarcodeCounts USING(sID)
+				                JOIN Experiments USING (eID)
+				                JOIN BindingSitesRNAP USING(sID)
+				          WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
+				                AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
+				          ORDER BY energy DESC LIMIT 1) as rnap
+				USING (pID) LIMIT 1;
+				"""
+
+		# Submit Query
+		results = self.__query(query, inputs)
+		return results
+
+
+
+	def get_promoter_expr_and_binding(self, promoter, condition, tf):
 		"""Get Promoter Expression and TF/RNAP Binding
 
 		Args:
 			promoter: Name of promoter sequence to query
 			condition: Condition in which to grab expression values
 			tf: Name of TF to get binding
-			include_rnap: Boolean to include RNAP binding
 
 		Returns:
 			Dictionary of {results, colnames, rowcount}
@@ -119,35 +179,76 @@ class RegSeqDB:
 
 		# Define inputs and query
 		inputs = [promoter, condition, tf]
+		query = """
+				SELECT sID, SUM(num_DNA) as num_DNA, SUM(num_RNA) as num_RNA, energy, affinity FROM Promoters
+					JOIN PromoterSequences AS ps USING(pID)
+					JOIN BarcodeCounts USING(sID)
+					JOIN Experiments USING (eID)
+					JOIN BindingSitesRNAP using(sID)
+					JOIN BindingSitesTF USING(sID)
+					JOIN TranscriptionFactors USING(tID)
+				WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
+					AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
+					AND tID = (SELECT tID FROM TranscriptionFactors WHERE tf_name = %s)
+				GROUP BY ps.seq;
+				"""
 
-		# Include RNAP Binding
-		if include_rnap:
-			query = """
-					SELECT sID, SUM(num_DNA) as num_DNA, SUM(num_RNA) as num_RNA, energy, affinity FROM Promoters
-						JOIN PromoterSequences AS ps USING(pID)
-						JOIN BarcodeCounts USING(sID)
-						JOIN Experiments USING (eID)
-						JOIN BindingSitesRNAP using(sID)
-						LEFT JOIN BindingSitesTF USING(sID)
-						JOIN TranscriptionFactors USING(tID)
-					WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
-						AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
-						AND tID = (SELECT tID FROM TranscriptionFactors WHERE tf_name = %s)
-					GROUP BY ps.seq;
-					"""
-		else:
-			query = """
-					SELECT sID, SUM(num_DNA) as num_DNA, SUM(num_RNA) as num_RNA, affinity FROM Promoters
-						JOIN PromoterSequences AS ps USING(pID)
-						JOIN BarcodeCounts USING(sID)
-						JOIN Experiments USING (eID)
-						JOIN BindingSitesTF USING(sID)
-						JOIN TranscriptionFactors USING(tID)
-					WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
-						AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
-						AND tID = (SELECT tID FROM TranscriptionFactors WHERE tf_name = %s)
-					GROUP BY ps.seq;
-					"""
+		# Submit Query
+		results = self.__query(query, inputs)
+		return results
+
+
+	def get_condition_comparison(self, promoter, tf, condition1, condition2):
+		"""Get Promoter LogFC and TF/RNAP Binding comparing across conditions
+
+		Args:
+			promoter: Name of promoter sequence to query
+			tf: Name of TF to get binding
+			condition1: First condition in which to grab expression values
+			condition2: Second condition in which to grab expression values
+
+		Returns:
+			Dictionary of {results, colnames, rowcount}
+		"""
+
+		# Check Promoter Value
+		if not utils.db_contains(self.cursor, "Promoters", "pro_name", promoter):
+			raise ValueError(f"ERROR: Promoter {promoter} not found in Promoters table.")
+		# Check Condition1 Value
+		if not utils.db_contains(self.cursor, "Experiments", "cond", condition1):
+			raise ValueError(f"ERROR: Condition {condition1} not found in Experiments table.")
+		# Check Condition2 Value
+		if not utils.db_contains(self.cursor, "Experiments", "cond", condition2):
+			raise ValueError(f"ERROR: Condition {condition2} not found in Experiments table.")
+		# Check TF Value
+		if not utils.db_contains(self.cursor, "TranscriptionFactors", "tf_name", tf):
+			raise ValueError(f"ERROR: TF {tf} not found in TranscriptionFactors table.")
+
+		# Define inputs and query
+		inputs = [promoter, condition1, tf, promoter, condition2]
+		query = """
+				SELECT sID, e1.num_DNA as Cond1_DNA, e1.num_RNA as Cond1_RNA, e2.num_DNA as Cond2_DNA, e2.num_RNA as Cond2_RNA, energy, affinity
+				FROM (SELECT sID, SUM(num_DNA) as num_DNA, SUM(num_RNA) as num_RNA, energy, affinity FROM Promoters
+				                JOIN PromoterSequences AS ps USING(pID)
+				                JOIN BarcodeCounts USING(sID)
+				                JOIN Experiments USING (eID)
+				                JOIN BindingSitesRNAP using(sID)
+				                JOIN BindingSitesTF USING(sID)
+				                JOIN TranscriptionFactors USING(tID)
+				        WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
+				              AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
+				              AND tID = (SELECT tID FROM TranscriptionFactors WHERE tf_name = %s)
+				        GROUP BY ps.seq) as e1
+				JOIN (SELECT sID, SUM(num_DNA) as num_DNA, SUM(num_RNA) as num_RNA FROM Promoters
+				                JOIN PromoterSequences AS ps USING(pID)
+				                JOIN BarcodeCounts USING(sID)
+				                JOIN Experiments USING (eID)
+				        WHERE pID = (SELECT pID FROM Promoters WHERE pro_name = %s)
+				              AND eID = (SELECT eID FROM Experiments WHERE cond = %s LIMIT 1)
+				        GROUP BY ps.seq) as e2
+				USING (sID);
+
+				"""
 
 		# Submit Query
 		results = self.__query(query, inputs)
